570faf7de5bf3cb712ff55e648474f535c287076
Joni De Borger <joni.de.borger@gmail.com>
2020-02-18 18:52:04 +0100

    solutions

--- 02_wordjuggling.py
+++ 02_wordjuggling.py
@@ -1,9 +1,18 @@
 sentence = "an exercise about nothing but lists"
 
-#implement these functions as described
+def return_the_first_and_last_word(sentence):
+    return [ sentence[0], sentence[-1] ]
+
+def sort_list(sentence):
+    sentence.sort()
+    return sentence
+
+def split_sentence(sentence):
+    return sentence.split()
 
 sentence = split_sentence(sentence)
 sentence = sort_list(sentence)
 sentence = return_the_first_and_last_word(sentence)
+
 print(sentence)
-#sort_list(sentence)
+#['about', 'nothing']
--- 03_readwrite.py
+++ 03_readwrite.py
@@ -7,19 +7,48 @@
 #JSON reference https://www.programiz.com/python-programming/json
 
 
-
 #1 import the json module
+import urllib.request
+import json 
+import csv
+
+astronauts = []
+astronauts_in_space = []
+
+astronaut_combined = []
 
 #2 consume the api
+with urllib.request.urlopen("http://api.open-notify.org/astros.json") as url:
+    data = json.loads(url.read().decode())
+    persons = data.get("people")
+
+    print(persons)
+
+    for person in persons:
+        astronauts_in_space.append(person.get('name'))
+    
+    print(astronauts_in_space)
 
 #3 use csv.reader() to read the astronauts file
-
+with open('astronauts.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for row in reader:
+        astronauts.append({
+            'name': row[0],
+            'year': row[1],
+            'flights': row[4]
+        })
 
 #4 dictionary magic
-
+for astronaut_in_space in astronauts_in_space:
+    for astronaut in astronauts:
+        if(astronaut.get('name') == astronaut_in_space):
+            # print("Have astronaut")
+            astronaut_combined.append(astronaut)
 
 #5 save the json in a dictionary
 
 #6 use json.dump to save a new file
-
+with open("astronauts_in_space.json", "w") as write_file:
+    json.dump(astronaut_combined, write_file)
 
